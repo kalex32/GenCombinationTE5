cf086c99376512f6c7661384687ca2b5e5780971
Olexiy Koval <kalex32@ukr.net>
2018-12-24 15:11:20 +0200

    the method of checking created. necessary to add a database

--- src/Gen_Random_TE5/CheckComb.java
+++ src/Gen_Random_TE5/CheckComb.java
@@ -1,12 +1,23 @@
 package Gen_Random_TE5;
 
 class CheckComb {
-//    static boolean check (int combination[]){
-//        for (int i = 0; i < combination.length; i++) {
-//            if (Math.abs(combination[i]-combination[i+1])>4){
-//
-//            }
-//        }
-//        return boolean;
-//    }
+
+    private static int jump = 5;
+
+    static void setJump(int jump) {
+        CheckComb.jump = jump;
+    }
+
+    static boolean check(int combination[]) {
+        for (int i = 0; i < combination.length - 1; i++) {
+            if (Math.abs(combination[i] - combination[i + 1]) < jump) {
+                for (int j = 0; j < combination.length - 2; j++) {
+                    if (combination[j] != combination[j + 1] && combination[j] != combination[j + 2]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/Gen_Random_TE5/GenRandom.java
+++ src/Gen_Random_TE5/GenRandom.java
@@ -6,7 +6,7 @@ class GenRandom {
     static int[] comb() {
         int[] combination = new int[5];
         for (int i = 0; i < combination.length; i++) {
-            combination[i] = (int) (Math.random() * 10);
+            combination[i] = (int) ((Math.random() * 10) + 1);
         }
         return combination;
     }
