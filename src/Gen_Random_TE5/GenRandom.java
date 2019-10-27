883f39d6591689c8fa7058fcb07771a817433738
Olexiy Koval <kalex32@ukr.net>
2019-10-28 00:52:22 +0200

    Derived in separate methods - does not work

--- src/Gen_Random_TE5/GenRandom.java
+++ src/Gen_Random_TE5/GenRandom.java
@@ -3,14 +3,23 @@ package Gen_Random_TE5;
 import java.util.Scanner;
 
 class GenRandom {
-
-
-    static int[] comb() {
+    static int maxPin() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите максимальную высоту пина: ");
         int maxPin = scanner.nextInt();
+        scanner.close();
+        return maxPin;
+    }
+    static int minPin(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите минимальную высоту пина: ");
-        int minPin = scanner.nextInt()-1;
+        int minPin = scanner.nextInt();
+        scanner.close();
+        return minPin;
+    }
+
+    static int[] comb(int maxPin, int minPin) {
+
         int[] combination = new int[5];
         for (int i = 0; i < combination.length; i++) {
             combination[i] = maxPin + (int) (Math.random() * minPin);
--- src/Gen_Random_TE5/Main.java
+++ src/Gen_Random_TE5/Main.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] comb1;
+        int maxPin = GenRandom.maxPin();
+        int minPin = GenRandom.minPin();
         do {
-            comb1 = GenRandom.comb();
+            comb1 = GenRandom.comb(maxPin, minPin);
             System.out.println(Arrays.toString(comb1));
 
         } while (!CheckComb.check(comb1));
